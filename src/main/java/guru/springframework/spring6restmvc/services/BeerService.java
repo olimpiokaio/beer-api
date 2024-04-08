cbca70b44ab858b13240d5f5955a26f3ae9eeb45
Olimpio.Silva <olimpiocode@gmail.com>
2024-04-08 16:52:02 -0300

    subindo api beer

---
+++ src/main/java/guru/springframework/spring6restmvc/services/BeerService.java
@@ -0,0 +1,26 @@
+package guru.springframework.spring6restmvc.services;
+
+import guru.springframework.spring6restmvc.model.Beer;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by jt, Spring Framework Guru.
+ */
+@Service
+public interface BeerService {
+
+    List<Beer> listBeers();
+
+    Beer getBeerById(UUID id);
+
+    Beer saveNewBeer(Beer beer);
+
+    void updateBeerById(UUID beerId, Beer beer);
+
+    void deleteById(UUID beerId);
+
+    void patchBeerById(UUID beerId, Beer beer);
+}
